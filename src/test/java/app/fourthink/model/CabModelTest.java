7f8d19b23f809c1315fa4d39f89c1a0d20972db0
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2012-02-29 19:19:42 -0300

    test: cover cab model construction and display

---
+++ src/test/java/app/fourthink/model/CabModelTest.java
@@ -0,0 +1,29 @@
+package app.fourthink.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class CabModelTest {
+
+    @Test
+    public void storesMakeModelAndCategory() {
+        CabModel m = new CabModel("Volkswagen", "Gol", CabCategory.NORMAL);
+        assertEquals("Volkswagen", m.getMake());
+        assertEquals("Gol", m.getModel());
+        assertEquals(CabCategory.NORMAL, m.getCategory());
+    }
+
+    @Test
+    public void newInstanceHasNoIdentity() {
+        CabModel m = new CabModel("Fiat", "Palio", CabCategory.NORMAL);
+        assertNull(m.getId());
+    }
+
+    @Test
+    public void displayNameCombinesMakeAndModel() {
+        CabModel m = new CabModel("Toyota", "Corolla", CabCategory.CONFORTO);
+        assertEquals("Toyota Corolla", m.displayName());
+    }
+}
