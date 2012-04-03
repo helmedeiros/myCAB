cdd8d5fe1bb4d823845611570bb65f465ffdd60c
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2012-04-02 21:25:19 -0300

    feat: declare cab repository

---
+++ src/main/java/app/fourthink/persistence/CabRepository.java
@@ -0,0 +1,23 @@
+package app.fourthink.persistence;
+
+import app.fourthink.model.Cab;
+import app.fourthink.model.CabStatus;
+
+import java.util.List;
+
+public interface CabRepository {
+
+    Cab save(Cab cab);
+
+    Cab findById(Long id);
+
+    Cab findByPlate(String plate);
+
+    List<Cab> findAll();
+
+    List<Cab> findByStatus(CabStatus status);
+
+    void delete(Cab cab);
+
+    long count();
+}
