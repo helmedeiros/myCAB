8a33357d02128896f3ddf0fb390ec6f09cd1040c
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2012-04-20 20:18:45 -0300

    feat: add driver entity

---
+++ src/main/java/app/fourthink/model/Driver.java
@@ -0,0 +1,79 @@
+package app.fourthink.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "driver")
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(name = "license_number", nullable = false, unique = true)
+    private String licenseNumber;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "preferred_category", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CabCategory preferredCategory;
+
+    public Driver() {
+    }
+
+    public Driver(String fullName, String email, Phone phone, String licenseNumber,
+                  String passwordHash, CabCategory preferredCategory) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone.getValue();
+        this.licenseNumber = licenseNumber;
+        this.passwordHash = passwordHash;
+        this.preferredCategory = preferredCategory;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Phone getPhone() {
+        return new Phone(phone);
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public CabCategory getPreferredCategory() {
+        return preferredCategory;
+    }
+}
