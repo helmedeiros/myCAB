bf9fbd3c3b327782f00a14a824d186237909c09a
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2012-05-04 19:41:28 -0300

    feat: implement dispatch repository on jpa

---
+++ src/main/java/app/fourthink/persistence/JpaDispatchRepository.java
@@ -0,0 +1,61 @@
+package app.fourthink.persistence;
+
+import app.fourthink.model.Dispatch;
+import app.fourthink.model.DispatchStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+@Transactional
+public class JpaDispatchRepository implements DispatchRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Dispatch save(Dispatch dispatch) {
+        if (dispatch.getId() == null) {
+            em.persist(dispatch);
+            return dispatch;
+        }
+        return em.merge(dispatch);
+    }
+
+    @Override
+    public Dispatch findById(Long id) {
+        return em.find(Dispatch.class, id);
+    }
+
+    @Override
+    public List<Dispatch> findActive() {
+        TypedQuery<Dispatch> q = em.createQuery(
+                "select d from Dispatch d where d.status in :s order by d.createdAt desc",
+                Dispatch.class);
+        q.setParameter("s", Arrays.asList(DispatchStatus.REQUESTED, DispatchStatus.ASSIGNED));
+        return q.getResultList();
+    }
+
+    @Override
+    public List<Dispatch> findByCabId(Long cabId) {
+        TypedQuery<Dispatch> q = em.createQuery(
+                "select d from Dispatch d where d.assignedCab.id = :id order by d.createdAt desc",
+                Dispatch.class);
+        q.setParameter("id", cabId);
+        return q.getResultList();
+    }
+
+    @Override
+    public List<Dispatch> findByCustomerId(Long customerId) {
+        TypedQuery<Dispatch> q = em.createQuery(
+                "select d from Dispatch d where d.customer.id = :id order by d.createdAt desc",
+                Dispatch.class);
+        q.setParameter("id", customerId);
+        return q.getResultList();
+    }
+}
