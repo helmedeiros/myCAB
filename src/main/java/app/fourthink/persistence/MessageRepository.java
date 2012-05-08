582ce6f6cb3123a73fed45cca264e6154a160fa3
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2012-05-08 18:26:15 -0300

    feat: declare message repository

---
+++ src/main/java/app/fourthink/persistence/MessageRepository.java
@@ -0,0 +1,15 @@
+package app.fourthink.persistence;
+
+import app.fourthink.model.Message;
+import app.fourthink.model.RecipientKind;
+
+import java.util.List;
+
+public interface MessageRepository {
+
+    Message save(Message message);
+
+    List<Message> findUnread(RecipientKind kind, Long recipientId);
+
+    List<Message> findRecent(RecipientKind kind, Long recipientId, int limit);
+}
