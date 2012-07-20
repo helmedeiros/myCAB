85df79ed615ec80d056b34cd4c99980b81bb954c
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2012-07-20 19:24:26 -0300

    style: keep imports separated cleanly

--- src/main/java/app/fourthink/service/DispatchService.java
+++ src/main/java/app/fourthink/service/DispatchService.java
@@ -10,6 +10,7 @@ import app.fourthink.model.RecipientKind;
 import app.fourthink.persistence.CabRepository;
 import app.fourthink.persistence.CustomerRepository;
 import app.fourthink.persistence.DispatchRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
