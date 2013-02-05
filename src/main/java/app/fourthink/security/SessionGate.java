e56adf6adbe164c429498e997a8bf1259e6a823d
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-02-05 18:52:13 -0300

    feat: session gate carries customer id alongside driver id

--- src/main/java/app/fourthink/security/SessionGate.java
+++ src/main/java/app/fourthink/security/SessionGate.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 public final class SessionGate {
 
     public static final String DRIVER_ID = "driverId";
+    public static final String CUSTOMER_ID = "customerId";
 
     private SessionGate() {}
 
@@ -14,6 +15,15 @@ public final class SessionGate {
 
     public static Long driverId(HttpSession session) {
         Object id = session.getAttribute(DRIVER_ID);
+        return id instanceof Long ? (Long) id : null;
+    }
+
+    public static boolean isCustomerAuthenticated(HttpSession session) {
+        return session != null && session.getAttribute(CUSTOMER_ID) != null;
+    }
+
+    public static Long customerId(HttpSession session) {
+        Object id = session.getAttribute(CUSTOMER_ID);
         return id instanceof Long ? (Long) id : null;
     }
 }
