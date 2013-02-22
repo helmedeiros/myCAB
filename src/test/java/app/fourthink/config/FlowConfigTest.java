cd24506969396ffc0f6a18c91aa4a78c73c1ed5e
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-02-22 18:34:35 -0300

    test: cover flow config defaults and enabled count

---
+++ src/test/java/app/fourthink/config/FlowConfigTest.java
@@ -0,0 +1,41 @@
+package app.fourthink.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext-test.xml")
+public class FlowConfigTest {
+
+    @Autowired
+    private FlowConfig config;
+
+    @Test
+    public void loadsDefaultsFromFlowsProperties() {
+        assertTrue(config.isPhoneCallEnabled());
+        assertTrue(config.isRequestEnabled());
+        assertFalse(config.isAutoDispatchEnabled());
+        assertEquals(3, config.getAutoDispatchDeclineCap());
+    }
+
+    @Test
+    public void enabledCountReflectsToggles() {
+        assertEquals(2, config.getEnabledCount());
+        assertTrue(config.isAnyEnabled());
+    }
+
+    @Test
+    public void manualBuildEchoesValues() {
+        FlowConfig only = new FlowConfig(false, false, true, 5);
+        assertEquals(1, only.getEnabledCount());
+        assertEquals(5, only.getAutoDispatchDeclineCap());
+        assertTrue(only.isAutoDispatchEnabled());
+    }
+}
