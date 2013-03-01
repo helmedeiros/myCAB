1fb5b966b11bad4923e189ecf25a371ba73989c9
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-03-01 20:05:58 -0300

    feat: add call operator service emitting customer-tagged messages

---
+++ src/main/java/app/fourthink/service/CallOperatorService.java
@@ -0,0 +1,45 @@
+package app.fourthink.service;
+
+import app.fourthink.config.FlowConfig;
+import app.fourthink.model.Customer;
+import app.fourthink.model.Message;
+import app.fourthink.model.RecipientKind;
+import app.fourthink.persistence.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CallOperatorService {
+
+    static final Long OPERATOR_INBOX = 0L;
+
+    private CustomerRepository customers;
+    private MessagingService messaging;
+    private FlowConfig flows;
+
+    public CallOperatorService() {
+    }
+
+    @Autowired
+    public CallOperatorService(CustomerRepository customers, MessagingService messaging,
+                                FlowConfig flows) {
+        this.customers = customers;
+        this.messaging = messaging;
+        this.flows = flows;
+    }
+
+    public Message requestCall(Long customerId) {
+        if (!flows.isPhoneCallEnabled()) {
+            throw new IllegalStateException("call-operator flow is disabled");
+        }
+        Customer customer = customers.findById(customerId);
+        if (customer == null) {
+            throw new IllegalArgumentException("unknown customer: " + customerId);
+        }
+        String body = customer.getName() + " (" + customer.getPhone().getValue()
+                + ") pediu para falar com a central.";
+        return messaging.send(RecipientKind.OPERATOR, OPERATOR_INBOX, body, customer.getId());
+    }
+}
