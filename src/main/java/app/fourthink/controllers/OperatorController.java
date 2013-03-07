24286a075e337db1c73e55d6afe1c120a1cc0cbb
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-03-06 21:28:40 -0300

    feat: operator console loads incoming customer calls

--- src/main/java/app/fourthink/controllers/OperatorController.java
+++ src/main/java/app/fourthink/controllers/OperatorController.java
@@ -1,34 +1,53 @@
 package app.fourthink.controllers;
 
+import app.fourthink.config.FlowConfig;
 import app.fourthink.model.CabCategory;
+import app.fourthink.model.Customer;
+import app.fourthink.model.Message;
 import app.fourthink.persistence.CabRepository;
+import app.fourthink.persistence.CustomerRepository;
 import app.fourthink.service.CustomerService;
 import app.fourthink.service.DispatchService;
 import app.fourthink.service.DriverReviewService;
+import app.fourthink.service.MessagingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/operator")
 public class OperatorController {
 
+    static final Long OPERATOR_INBOX = 0L;
+
     private final DispatchService dispatches;
     private final CabRepository cabs;
     private final CustomerService customers;
     private final DriverReviewService review;
+    private final MessagingService messaging;
+    private final CustomerRepository customerRepository;
+    private final FlowConfig flows;
 
     @Autowired
     public OperatorController(DispatchService dispatches, CabRepository cabs,
-                               CustomerService customers, DriverReviewService review) {
+                               CustomerService customers, DriverReviewService review,
+                               MessagingService messaging,
+                               CustomerRepository customerRepository,
+                               FlowConfig flows) {
         this.dispatches = dispatches;
         this.cabs = cabs;
         this.customers = customers;
         this.review = review;
+        this.messaging = messaging;
+        this.customerRepository = customerRepository;
+        this.flows = flows;
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -40,6 +59,25 @@ public class OperatorController {
         List<?> pending = review.pending();
         model.addAttribute("pendingDrivers", pending);
         model.addAttribute("pendingCount", pending.size());
+        model.addAttribute("flows", flows);
+        model.addAttribute("operatorCalls", incomingCalls());
         return "operator/home";
+    }
+
+    private List<Map<String, Object>> incomingCalls() {
+        List<Map<String, Object>> out = new ArrayList<Map<String, Object>>();
+        if (!flows.isPhoneCallEnabled()) {
+            return out;
+        }
+        for (Message m : messaging.pendingOperatorCalls(OPERATOR_INBOX)) {
+            Map<String, Object> entry = new LinkedHashMap<String, Object>();
+            entry.put("id", m.getId());
+            entry.put("body", m.getBody());
+            entry.put("createdAt", m.getCreatedAt());
+            Customer customer = customerRepository.findById(m.getSourceCustomerId());
+            entry.put("customer", customer);
+            out.add(entry);
+        }
+        return out;
     }
 }
