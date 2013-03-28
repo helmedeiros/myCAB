d4c17bb7930ed57b26bc257e887805db6278518f
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-03-28 19:45:02 -0300

    feat: nearest cab finder can exclude previously declined cabs

--- src/main/java/app/fourthink/service/NearestCabFinder.java
+++ src/main/java/app/fourthink/service/NearestCabFinder.java
@@ -9,7 +9,10 @@ import app.fourthink.persistence.CabRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class NearestCabFinder {
@@ -22,12 +25,19 @@ public class NearestCabFinder {
     }
 
     public Cab findClosest(Location pickup, CabCategory category) {
+        return findClosestExcluding(pickup, category, Collections.<Long>emptySet());
+    }
+
+    public Cab findClosestExcluding(Location pickup, CabCategory category,
+                                     java.util.Collection<Long> excludedCabIds) {
+        Set<Long> exclusions = new HashSet<Long>(excludedCabIds);
         List<Cab> free = cabs.findByStatus(CabStatus.FREE);
         Cab best = null;
         double bestKm = Double.MAX_VALUE;
         for (Cab cab : free) {
             if (cab.getModel().getCategory() != category) continue;
             if (!cab.hasLocation()) continue;
+            if (exclusions.contains(cab.getId())) continue;
             double km = Distance.kilometers(pickup, cab.getLocation());
             if (km < bestKm) {
                 bestKm = km;
