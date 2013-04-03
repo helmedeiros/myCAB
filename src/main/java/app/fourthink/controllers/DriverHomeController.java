cd351061340b2a3043e9b9905ac9e700f6a5d1db
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-04-03 19:29:03 -0300

    feat: driver home shows active proposal with accept and decline routes

--- src/main/java/app/fourthink/controllers/DriverHomeController.java
+++ src/main/java/app/fourthink/controllers/DriverHomeController.java
@@ -6,6 +6,8 @@ import app.fourthink.model.DispatchStatus;
 import app.fourthink.model.Driver;
 import app.fourthink.persistence.DriverRepository;
 import app.fourthink.security.SessionGate;
+import app.fourthink.persistence.DispatchRepository;
+import app.fourthink.service.AutoDispatchService;
 import app.fourthink.service.DispatchService;
 import app.fourthink.service.FleetService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,13 +28,19 @@ public class DriverHomeController {
     private final DriverRepository drivers;
     private final DispatchService dispatches;
     private final FleetService fleet;
+    private final DispatchRepository dispatchRepository;
+    private final AutoDispatchService autoDispatch;
 
     @Autowired
     public DriverHomeController(DriverRepository drivers, DispatchService dispatches,
-                                 FleetService fleet) {
+                                 FleetService fleet,
+                                 DispatchRepository dispatchRepository,
+                                 AutoDispatchService autoDispatch) {
         this.drivers = drivers;
         this.dispatches = dispatches;
         this.fleet = fleet;
+        this.dispatchRepository = dispatchRepository;
+        this.autoDispatch = autoDispatch;
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -43,7 +51,8 @@ public class DriverHomeController {
         Driver driver = drivers.findById(SessionGate.driverId(session));
         model.addAttribute("driver", driver);
         if (driver.getCab() != null) {
-            List<Dispatch> recent = dispatches.forCab(driver.getCab().getId());
+            Long cabId = driver.getCab().getId();
+            List<Dispatch> recent = dispatches.forCab(cabId);
             Dispatch current = null;
             for (Dispatch d : recent) {
                 if (d.getStatus() == DispatchStatus.ASSIGNED) {
@@ -52,8 +61,39 @@ public class DriverHomeController {
                 }
             }
             model.addAttribute("currentDispatch", current);
+            model.addAttribute("proposal", findProposalFor(cabId));
         }
         return "drivers/home";
+    }
+
+    private Dispatch findProposalFor(Long cabId) {
+        for (Dispatch d : dispatchRepository.findPendingCustomerRequests()) {
+            if (d.getStatus() == DispatchStatus.PROPOSED
+                    && cabId.equals(d.getProposedCabId())) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/proposal/{id}/accept", method = RequestMethod.POST)
+    public String acceptProposal(HttpSession session, @org.springframework.web.bind.annotation.PathVariable Long id) {
+        if (!SessionGate.isAuthenticated(session)) {
+            return "redirect:/login";
+        }
+        Driver driver = drivers.findById(SessionGate.driverId(session));
+        autoDispatch.accept(id, driver.getCab().getId());
+        return "redirect:/driver";
+    }
+
+    @RequestMapping(value = "/proposal/{id}/decline", method = RequestMethod.POST)
+    public String declineProposal(HttpSession session, @org.springframework.web.bind.annotation.PathVariable Long id) {
+        if (!SessionGate.isAuthenticated(session)) {
+            return "redirect:/login";
+        }
+        Driver driver = drivers.findById(SessionGate.driverId(session));
+        autoDispatch.decline(id, driver.getCab().getId());
+        return "redirect:/driver";
     }
 
     @RequestMapping(value = "/online", method = RequestMethod.POST)
